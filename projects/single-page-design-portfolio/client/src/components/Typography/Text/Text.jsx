b40b2d17f9edec6b84c2e5761bd1eb51c850263d
Alex Sherbin <contact.alexsherbin@gmail.com>
2022-10-28 19:54:11 -0400

    refactor-2

--- projects/single-page-design-portfolio/client/src/components/Typography/Text/Text.jsx
+++ projects/single-page-design-portfolio/client/src/components/Typography/Text/Text.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 
 // styles
 import styles from './Text.module.css';
 
-const cx = classNames.bind(styles);
-
 export const Text = ({ children, tag = 'h2', size = 'md', color = 'dark' }) => {
   const Component = tag;
-  const className = cx({
-    [size]: true,
-    [color]: true,
-    text: true,
-  });
 
-  return <Component className={className}>{children}</Component>;
+  return (
+    <Component className={`${styles[color]} ${styles[size]} ${styles.text}`}>
+      {children}
+    </Component>
+  );
 };
 
 Text.propTypes = {
